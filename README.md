e11d64300d6ff97c1959bf5c7d3863ede5ff4858
David Sac <saccsaradavid@gmail.com>
2025-08-01 22:23:22 -0500

    Mejora de archivos con totales y limpieza de nombres, README

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Generador de Reporte Automáticos en Excel
 
-Este proyecto autommatiza la lectura de datos desde un archivo Excel y genera reportes individuales por categoría (ej. por producto, región, etc.)c
+Este proyecto autommatiza la lectura de datos desde un archivo Excel y genera reportes individuales por categoría (ej. por producto, región, etc.).
+
+## Funcionalidades
+
+- Lee datos desde un archivo Excel
+- Agrupa por producto
+- Calcula totales por producto (unidades y ventas)
+- Genera un archivo Excel por producto en la carpeta 'reportes/'
+
+## Tecnologías usadas
+
+- Python 3
+- Pandas
+- OpenPyXL
+
+## Ejecución
+
+1. Coloca tu archivo dentro de la carpeta 'data/'
+2. Ejecuta el script: cd src, python main.py
+
--- src/main.py
+++ src/main.py
@@ -5,15 +5,35 @@ ruta_archivo = './data/ventas.xlsx'
 
 df = pd.read_excel(ruta_archivo)
 
-ruta_reportes = "./reportes"
-os.makedirs(ruta_reportes, exist_ok = True)
+df['Total']= df['Unidades'] * df['Precio Unitario']
+
+rutas_reportes ='./reportes'
+
+os.makedirs(rutas_reportes, exist_ok=True)
 
 productos = df['Producto'].unique()
 
 for producto in productos:
-    df_producto = df[df['Producto'] == producto]
-    ruta_salida = os.path.join(ruta_reportes, f"{producto}.xlsx")
-    df_producto.to_excel(ruta_salida, index = False)
-    print(f"Reporte generado: {ruta_salida}")
+    df_producto = df[df['Producto']== producto].copy()
     
-print("Todos los reportes fueron generados")
+    total_general = df_producto['Total'].sum()
+    
+    fila_total = pd.DataFrame([{
+        'Fecha': 'TOTAL',
+        'Región': '',
+        'Producto': '',
+        'Vendedor': '',
+        'Unidades': df_producto['Unidades'].sum(),
+        'Precio unitario': '',
+        'Total': total_general 
+    }])
+    
+    df_final = pd.concat([df_producto, fila_total], ignore_index=True)
+    
+    nombre_archivo = producto.replace(" ", "_") + ".xlsx"
+    ruta_salida = os.path.join(rutas_reportes, nombre_archivo)
+    
+    df_final.to_excel(ruta_salida, index=False)
+    print(f"Reporte generado: {ruta_salida}")
+
+print("Todos los reportes fueron generados con totales.")
